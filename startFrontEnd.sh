0e02ab23eec98424439c4f48387d0eeb3a9ffe90
zummino <71648648+zummino@users.noreply.github.com>
2023-02-03 11:22:52 +0100

    Update startFrontEnd.sh

--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -11,7 +11,7 @@ sudo apt install npm
 echo "Fine comandi installazione Node"
 
 echo "Inizio comandi esecuzione Frontend"
-cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/frontend
+cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master/src/main/ui
 echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
 npm install
 echo "Installazione npm effettuata, prossimo comando: npm start"
