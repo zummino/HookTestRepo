63f94574dbce93b126e61f3e149fbafcc5253f11
zummino <71648648+zummino@users.noreply.github.com>
2023-02-03 11:36:48 +0100

    Update startFrontEnd.sh

--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -11,6 +11,18 @@ sudo apt install npm
 echo "Fine comandi installazione Node"
 
 echo "Inizio comandi esecuzione Frontend"
+ls -a
+echo "cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master"
+cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master
+ls -a
+echo "cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master/src"
+cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master/src
+ls -a
+echo "cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master/main"
+cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master/src/main
+ls - a
+echo "cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master/src/main/ui"
+ls -a
 cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master/src/main/ui
 echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
 npm install
