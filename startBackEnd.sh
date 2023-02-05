41222c4f4bfda59e11c10be9312abcdee4ac781f
zummino <marcodeluca94@gmail.com>
2023-02-05 15:02:04 +0100

    prova1

--- startBackEnd.sh
+++ startBackEnd.sh
@@ -1,15 +1,15 @@
 echo "Inizio comandi installazione precondizioni"
-sudo apt update
-sudo apt install openjdk-11-jdk openjdk-11-jre
-echo "Installata versione di java numero"
+#sudo apt update
+#sudo apt install openjdk-11-jdk openjdk-11-jre
+#echo "Installata versione di java numero"
 java -version 
 
 echo "Inizio comandi esecuzione Backend"
-pwd
-ls -a
-cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master
-ls -a
-mvn clean install
+#pwd
+#ls -a
+#cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master
+#ls -a
+#mvn clean install
 cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master/target
 echo "Vediamo quali file jar si trovano in cartella target"
 ls -a
--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -1,30 +1,6 @@
-echo "Inizio comandi installazione precondizioni"
-cd /home/runner/work/HookTestRepo/HookTestRepo
-curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
-cat nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install nodejs
-echo "versione di node installata"
-node -v
-echo "Installazione di npm"
-sudo apt install npm
-echo "Fine comandi installazione Node"
-
 echo "Inizio comandi esecuzione Frontend"
-ls -a
-echo "cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master"
-cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master
-ls -a
-echo "cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master/src"
-cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master/src
-ls -a
-echo "cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master/main"
-cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master/src/main
-ls -a
-echo "cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/root/angular-java-example-master/src/main/ui"
-ls -a
 cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master/src/main/ui
 echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
-npm install
+#npm install
 echo "Installazione npm effettuata, prossimo comando: npm start"
 npm start &
