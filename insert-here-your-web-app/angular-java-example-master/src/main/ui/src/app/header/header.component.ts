6e725cf267d506eae01c09213088d6dc5401f362
zummino <marcodeluca94@gmail.com>
2023-02-27 10:13:49 +0100

    v_1h_2a_t2

--- insert-here-your-web-app/angular-java-example-master/src/main/ui/src/app/header/header.component.ts
+++ insert-here-your-web-app/angular-java-example-master/src/main/ui/src/app/header/header.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input,  Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AppService } from '../app.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
-  ngOnInit(): void {
+  users: any[] = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  @Input() userCount = 0;
+  @Output() getUsersEvent = new EventEmitter();
+
+
+ngOnInit() {
+	this.getAllUsers();
   }
+  
+  
+   getAllUsers() {
+    this.appService.getUsers().pipe(takeUntil(this.destroy$)).subscribe((users: any[]) => {
+		this.userCount = users.length;
+        this.users = users;
+		this.getUsersEvent.emit('get all users');
+    });
+  }
+  
+  
+   ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
 
 }
